d242a39d546a7c5d7dc0e2253cea4aad8012d459
Yuvraj Gupta <yuvraj19992@gmail.com>
2023-12-14 17:48:18 +0530

    final

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = express();
-const router = express.Router();
 const {authRouter, userRouter} = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
 
@@ -22,7 +21,6 @@ app.use(cors({
 app.use(express.json({limit:'25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: false}));
 
-router.get('/', postRouter);
 app.use('/auth', authRouter);
 app.use('/user', userRouter);
 app.use('/post', postRouter);
